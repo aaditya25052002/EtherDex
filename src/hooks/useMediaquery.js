c9358d9e5c8d888bc14e96a73c9bdba01dd2cfa5
Aditya Tiwari <pubgmlord25@gmail.com>
2023-07-18 20:57:37 +0530

    minor bug fixes

--- src/hooks/useMediaquery.js
+++ src/hooks/useMediaquery.js
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
 
 function useMediaQuery(query) {
-  const [matches, setMatches] = useState(window.matchMedia(query).matches);
+  const [matches, setMatches] = useState(false);
 
   useEffect(() => {
-    const media = window.matchMedia(query);
-    if (media.matches !== matches) {
-      setMatches(media.matches);
+    const isWindowDefined = typeof window !== "undefined";
+    if (isWindowDefined) {
+      const media = window.matchMedia(query);
+      if (media.matches !== matches) {
+        setMatches(media.matches);
+      }
+      const listener = () => setMatches(media.matches);
+      media.addEventListener("change", listener);
+      return () => media.removeEventListener("change", listener);
     }
-    const listener = () => setMatches(media.matches);
-    media.addEventListener("change", listener);
-    return () => media.removeEventListener("change", listener);
   }, [matches, query]);
 
   return matches;
